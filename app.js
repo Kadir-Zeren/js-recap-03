30f39072b8302de814dc0eaf093b85c3cce98d5c
kadir-zeren <k4dzeren@gmail.com>
2024-03-06 12:18:12 +0300

    ternary

--- app.js
+++ app.js
@@ -77,36 +77,55 @@
 //   console.log(`Your score:${result}`);
 // }
 
-const n1 = +prompt("Num1:");
-const n2 = +prompt("Num2:");
-const n3 = +prompt("Num3:");
+// const n1 = +prompt("Num1:");
+// const n2 = +prompt("Num2:");
+// const n3 = +prompt("Num3:");
 
-if (n1 > n2) {
-  if (n1 > n3) {
-    console.log("Biggest:", n1);
-  }
-}
+// if (n1 > n2) {
+//   if (n1 > n3) {
+//     console.log("Biggest:", n1);
+//   }
+// }
 
-if (n1 > n2 && n1 > n3) {
-  console.log("Biggest:", n1);
-} else if (n2 > n1 && n2 > n3) {
-  console.log("Biggest:", n2);
-} else if (n3 > n1 && n3 > n2) {
-  console.log("Biggesst:", n3);
-}
+// if (n1 > n2 && n1 > n3) {
+//   console.log("Biggest:", n1);
+// } else if (n2 > n1 && n2 > n3) {
+//   console.log("Biggest:", n2);
+// } else if (n3 > n1 && n3 > n2) {
+//   console.log("Biggesst:", n3);
+// }
 
-let biggest = n1;
+// let biggest = n1;
 
-if (n2 >= biggest) {
-  biggest = n2;
-}
-if (n3 >= biggest) {
-  biggest = n3;
-}
-console.log(`${biggest} is biggest`);
+// if (n2 >= biggest) {
+//   biggest = n2;
+// }
+// if (n3 >= biggest) {
+//   biggest = n3;
+// }
+// console.log(`${biggest} is biggest`);
 
 const grade1 = prompt("Please enter your grade:");
 
-grade1 >= 50 ? console.log("SUCCEED") : console.log("FAILED");
-const score = grade1 >= 50 ? "PASSED" : "FAILED";
-console.log(score);
+// grade1 >= 50 ? console.log("SUCCEED") : console.log("FAILED");
+// const score = grade1 >= 50 ? "PASSED" : "FAILED";
+// console.log(score);
+
+const score1 = grade1 >= 50 ? "SUCCEED" : "";
+console.log(score1);
+
+const speed = 110;
+
+speed > 120
+  ? console.log("Speedy")
+  : speed >= 90
+  ? console.log("Normal")
+  : console.log("Low Speed");
+
+if (speed > 120) {
+  console.log("Speedy");
+} else if (speed >= 90) {
+  console.log("Normal");
+} else {
+  console.log("Low Speed");
+}
