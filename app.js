1542aa6c36b6aca478486d3e198d6f813b5e4ed3
kadir-zeren <k4dzeren@gmail.com>
2024-03-06 11:52:47 +0300

    if else

---
+++ app.js
@@ -0,0 +1,78 @@
+// let sayi = prompt("Bir sayi girniz:");
+// console.log(typeof sayi);
+// let sayi2 = 10;
+// sayi = sayi + sayi2;
+
+// console.log(Number("123"));
+// console.log(Number("12.3"));
+// console.log(Number("0"));
+// console.log(Number(" "));
+// console.log(Number(""));
+// console.log(Number(null));
+// console.log(Number(undefined));
+
+// const myNumber = "threezeroone";
+// let myNumber2 = Number(myNumber);
+// console.log(myNumber2);
+// console.log(typeof myNumber2);
+
+// const dolar = "1000";
+// const tl = "2000";
+
+// const totalMoney = Number(dolar) + +tl;
+// console.log(totalMoney);
+
+// const dolarInt = parseInt(dolar);
+// console.log(dolarInt);
+// const tlFloat = parseFloat(tl);
+// console.log(tlFloat);
+
+// console.log(parseFloat(null));
+// console.log(parseFloat(""));
+// console.log(parseFloat(undefined));
+
+// const myNumber3 = 2000.5;
+// console.log(String(myNumber3));
+
+// const calismaSuresi = prompt("Calisma suresi");
+// let maas = prompt("Maasini giriniz");
+
+// if (calismaSuresi >= 10) {
+//   maas = Math.trunc(maas * 1.1);
+//   console.log("Zamli maasiniz:", maas);
+//   console.log(`Zamli maasiniz:, ${maas}`);
+// } else {
+//   console.log("Uzgunuz maasinaza zam yapamiyoruz", maas);
+// }
+
+// console.log("Gule gule");
+
+// if (calismaSuresi >= 10) {
+//   maas = Math.round(maas * 1.1);
+//   console.log(`Zamli maasiniz: ${maas}`);
+// }
+// console.log("Gule gule");
+
+// const grade = prompt("Please enter your grade");
+
+// let result = "";
+
+// if (grade < 0) {
+//   console.log("Grade can not be small than zero");
+// } else if (grade < 40) {
+//   result = "FF";
+// } else if (grade <= 50) {
+//   result = "DD";
+// } else if (grade <= 65) {
+//   result = "CC";
+// } else if (grade <= 89) {
+//   result = "BB";
+// } else if (grade <= 100) {
+//   result = "AA";
+// } else {
+//   console.log("Grade can not be bigger than 100");
+// }
+
+// if (result != "") {
+//   console.log(`Your score:${result}`);
+// }
